090cca866b24938e937e03726f5174c6492e11a8
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-01-18 09:39:47 +0200

    removing leftover code

--- flask.py
+++ flask.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('registracija.html')
+    return render_template('index.html')
 
-@app.route('/registret', methods=['GET'])
-def registret():
-    username = request.args.get('username')
-    password = request.args.get('password')
-
+#@app.route('/registret', methods=['GET'])
+#def registret():
+ #   username = request.args.get('username')
+ #   password = request.args.get('password')
+#
     # Saglabā lietotāja datus teksta failā
-    with open('lietotaji.txt', 'a') as file:
-        file.write(f'Lietotājvārds: {username}, Parole: {password}\n')
-
-    return 'Reģistrācija veiksmīga!'
+ #   with open('lietotaji.txt', 'a') as file:
+ #       file.write(f'Lietotājvārds: {username}, Parole: {password}\n')
+#
+ #   return 'Reģistrācija veiksmīga!'
 
 if __name__ == '__main__':
     app.run(debug=True)
