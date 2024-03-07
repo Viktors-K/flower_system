42d8981444f942865332b1424fbf64b2a813fab2
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-03-07 09:07:31 +0200

    added other info

---
+++ other/amomo.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('registracija.html')
+
+@app.route('/registret', methods=['GET'])
+def registret():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    # Saglabā lietotāja datus teksta failā
+    with open('lietotaji.txt', 'a') as file:
+        file.write(f'Lietotājvārds: {username}, Parole: {password}\n')
+
+    return 'Reģistrācija veiksmīga!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
