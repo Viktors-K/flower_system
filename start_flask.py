9b81ffc120eb75e7c10295c391a16124ae04c02c
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-04-08 09:40:28 +0300

    Added dashboard

--- start_flask.py
+++ start_flask.py
@@ -53,5 +53,9 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
